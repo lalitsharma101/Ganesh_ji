abfa20f2837451147d67728dc1f6b9d89f2ed50e
unknown <lalitsharmaji101@gmail.com>
2022-07-06 21:55:32 +0530

    header,footer and login page done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+<div class="row">
+    <div class="col-sm-4 col-sm-offset-4">
+        <form action="">
+        <div class="mb-3">
+            <label for="" class="form-label">Name</label>
+            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+            <label for="" class="form-label">Email</label>
+            <input type="email" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+          </div>
+        </form>
+    </div>
+</div>
+</div>
+@endsection
